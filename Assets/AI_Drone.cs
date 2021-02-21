583bf08231adaf790123973d0c4cc12bff5a0817
twpoko <tawan.hira@bumail.net>
2021-02-21 15:07:42 +0700

    [UPLOAD] Project

---
+++ Assets/AI_Drone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Drone : MonoBehaviour
+{
+    public float speed;
+    private Transform target;
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+    }
+}
