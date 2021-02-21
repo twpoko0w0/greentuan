99d4f5a3d9c6cc5416a95d11b25a431da27db212
twpoko <tawan.hira@bumail.net>
2021-02-21 15:40:46 +0700

    [UPLOAD] Project

---
+++ Assets/SpawnAstro.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAstro : MonoBehaviour
+{
+    public GameObject Astro;
+    private int xPos;
+    private int zPos;
+    private int yPos;
+    private int cubeCount = 0;
+    public int clone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CubeSp());
+    }
+
+    // Update is called once per frame
+
+    IEnumerator CubeSp()
+    {
+        while (cubeCount < clone)
+        {
+            Debug.Log("SpawnAstro");
+
+            xPos = Random.Range(-3000, 400);
+            zPos = Random.Range(1000, -2000);
+            yPos = Random.Range(-1200, 2400);
+            Instantiate(Astro, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(10f);
+            //Invoke("cubeCount += 1", 10f);
+            cubeCount += 1;
+
+
+        }
+    }
+}
---
+++ Assets/SpawnEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject Drone;
+    private int xPos;
+    private int zPos;
+    private int yPos;
+    public int cubeCount;
+    public int clone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CubeSp());
+    }
+
+    // Update is called once per frame
+
+    IEnumerator CubeSp()
+    {
+        while (cubeCount < clone)
+        {
+            Debug.Log("SpawnBitch!");
+
+            xPos = Random.Range(-700,2000 );
+            zPos = Random.Range(-1200, 1700);
+            yPos = Random.Range(-400, 700);
+            Instantiate(Drone, new Vector3(xPos, yPos, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(4f);
+            //Invoke("cubeCount += 1", 10f);
+            cubeCount += 1;
+
+
+        }
+    }
+}
